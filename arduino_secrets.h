bc317a1b41c7b4f59d0aea6fc92f86ddc8d6a427
Hussain AlRadhi <38873772+HA4ever37@users.noreply.github.com>
2020-12-26 19:30:25 -0500

    Delete arduino_secrets.h

--- arduino_secrets.h
+++
@@ -1,4 +0,0 @@
-#define SECRET_PINNUMBER     ""
-#define SECRET_GPRS_APN      "fp.com.attz" // replace your GPRS APN
-#define SECRET_GPRS_LOGIN    ""    // replace with your GPRS login
-#define SECRET_GPRS_PASSWORD "" // replace with your GPRS password
